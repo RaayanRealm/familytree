d20ef732f7b5d1d0820661000e5556635ad38740
RaayanRealm <raayany@outlook.com>
2025-05-29 20:45:53 +0000

    added db connection url for deployment on Render

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,15 +1,15 @@
 module.exports = {
   client: "pg",
-  connection: {
+  connection: process.env.DATABASE_URL || {
     host: "localhost",
     user: "family_admin",
     password: "raayan",
     database: "family_tree_db",
   },
-    migrations: {
-      directory: "./database/migrations",
-    },
-    seeds: {
-      directory: "./database/seeds",
-    },
+  migrations: {
+    directory: "./database/migrations",
+  },
+  seeds: {
+    directory: "./database/seeds",
+  },
 };
